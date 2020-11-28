ccaec718cf7cb1db560f33bfb51dac698fd8280a
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 14:14:33 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 import os
 import sys
 from zowe.zos_jobs_for_zowe_sdk import Jobs
-print sys.argv[1]
-print sys.argv[2]
-print sys.argv[3]
-print sys.argv[4]
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+print(sys.argv[4])
 
