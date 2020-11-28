5ea9b3d060d7a2af89631425d48e55edadc98a04
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 19:10:25 +0530

    Update build.py

--- build.py
+++ build.py
@@ -14,10 +14,22 @@ import sys
 #     "password": pwd,
 #     "ssl_verification": False
 # }
-
+##################################################################
+basepath = './'
+folders = os.listdir(basepath)
+ignored = ['.git','.github']
+#print(dir)
+for folder in folders:
+    path = os.path.join(basepath,folder)
+    if os.path.isdir(path):
+        if folder not in ignored:
+            print(folder)
+            for file in os.listdir(path):
+                print(file)
+##########################################################################
 # my_jobs = Jobs(connection)
 # result = my_jobs.submit_from_mainframe('RGOPALK.JCL.CNTL(RUN)')
 # time.sleep(15)
 # rc = my_jobs.get_job_status(result['jobname'],result['jobid'])
 # print(rc)
-print(os.listdir('./'))
+
