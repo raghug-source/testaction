9f0ea5726e8c23cd1729d39e4b4ea06556b1e2a0
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 17:07:29 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,22 +1,23 @@
 import os
 import time
 import sys
-from zowe.zos_jobs_for_zowe_sdk import Jobs
-hostname = sys.argv[1]
-hostport = sys.argv[2]
-host = hostname + ':' + hostport
-userid = sys.argv[3]
-pwd = sys.argv[4]
+# from zowe.zos_jobs_for_zowe_sdk import Jobs
+# hostname = sys.argv[1]
+# hostport = sys.argv[2]
+# host = hostname + ':' + hostport
+# userid = sys.argv[3]
+# pwd = sys.argv[4]
 
-connection = {
-    "host_url": host,
-    "user": userid,
-    "password": pwd,
-    "ssl_verification": False
-}
+# connection = {
+#     "host_url": host,
+#     "user": userid,
+#     "password": pwd,
+#     "ssl_verification": False
+# }
 
-my_jobs = Jobs(connection)
-result = my_jobs.submit_from_mainframe('RGOPALK.JCL.CNTL(RUN)')
-time.sleep(15)
-rc = my_jobs.get_job_status(result['jobname'],result['jobid'])
-print(rc)
+# my_jobs = Jobs(connection)
+# result = my_jobs.submit_from_mainframe('RGOPALK.JCL.CNTL(RUN)')
+# time.sleep(15)
+# rc = my_jobs.get_job_status(result['jobname'],result['jobid'])
+# print(rc)
+print(os.listdir('./'))
