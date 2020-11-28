c0017e6a5ba407d6ddebb679e9f7869f19c0660e
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 19:38:37 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,19 +1,21 @@
 import os
 import time
 import sys
-# from zowe.zos_jobs_for_zowe_sdk import Jobs
-# hostname = sys.argv[1]
-# hostport = sys.argv[2]
-# host = hostname + ':' + hostport
-# userid = sys.argv[3]
-# pwd = sys.argv[4]
+from zowe.zos_jobs_for_zowe_sdk import Jobs
+from zowe.zos_files_for_zowe_sdk import Files
+hostname = sys.argv[1]
+hostport = sys.argv[2]
+host = hostname + ':' + hostport
+userid = sys.argv[3]
+pwd = sys.argv[4]
 
-# connection = {
-#     "host_url": host,
-#     "user": userid,
-#     "password": pwd,
-#     "ssl_verification": False
-# }
+connection = {
+    "host_url": host,
+    "user": userid,
+    "password": pwd,
+    "ssl_verification": False
+}
+my_files = Files(connection)
 ##################################################################
 basepath = './'
 folders = os.listdir(basepath)
@@ -23,9 +25,12 @@ for folder in folders:
     path = os.path.join(basepath,folder)
     if os.path.isdir(path):
         if folder not in ignored:
-            print(folder)
+            pdstype = folder
             for file in os.listdir(path):
-                print(file)
+                memname = file.split('.')[0]
+                localfile = './' + folder + '/' + file
+                pdsname = 'RGOPALK.RAG.' + pdstype + '(' + memname + ')'
+                my_files.upload_file_to_dsn(localfile,pdsname)
 ##########################################################################
 # my_jobs = Jobs(connection)
 # result = my_jobs.submit_from_mainframe('RGOPALK.JCL.CNTL(RUN)')
