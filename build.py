154833833bf188f04dc3168bbe86c333be618fa9
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 11:58:36 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 import os
 import sys
-command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
+command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(WHELLO)' --view-all-spool-content"
 #command = "zowe --version"
 os.system(command)
