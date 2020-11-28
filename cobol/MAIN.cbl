a9a26fe0ead196fd2aa7637fae4a159c6bdafeaf
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-28 17:58:42 +0530

    Create MAIN.cbl

---
+++ cobol/MAIN.cbl
@@ -0,0 +1,12 @@
+IDENTIFICATION DIVISION.                      
+PROGRAM-ID.     MAIN.                         
+                                              
+DATA DIVISION.                                
+   WORKING-STORAGE SECTION.                   
+   COPY STUDCPY.                              
+PROCEDURE DIVISION.                           
+    CALL 'UTIL' USING BY CONTENT WS-STUDENT-ID
+                BY CONTENT WS-STUDENT-NAME.   
+    DISPLAY 'STUDENT ID:' WS-STUDENT-ID       
+    DISPLAY 'STUDENT-NAME:' WS-STUDENT-NAME   
+    STOP RUN.                                 
