a83df56be235b47ccf8a96e45d1d26cfefab6145
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-30 22:52:43 +0530

    Update MAIN.cbl

--- cobol/MAIN.cbl
+++ cobol/MAIN.cbl
@@ -6,7 +6,7 @@
        COPY STUDCPY.                              
       PROCEDURE DIVISION.                           
       CALL 'UTIL' USING BY CONTENT WS-STUDENT-ID
-                BY CONTENT WS-STUDENT-NAME.   
+                BY CONTENT FS-STUDENT-NAME.   
       DISPLAY 'STUDENT FID:' WS-STUDENT-ID       
       DISPLAY 'STUDENT-FNAME:' WS-STUDENT-NAME   
       STOP RUN.                                 
