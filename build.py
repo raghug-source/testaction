3d8646d1bb9e643917f0794bf3ec90110a318fce
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-01 07:40:54 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,43 +1,8 @@
 import os
-import time
 import sys
-from zowe.zos_jobs_for_zowe_sdk import Jobs
-from zowe.zos_files_for_zowe_sdk import Files
-hostname = sys.argv[1]
-hostport = sys.argv[2]
-host = hostname + ':' + hostport
-userid = sys.argv[3]
-pwd = sys.argv[4]
-
-connection = {
-    "host_url": host,
-    "user": userid,
-    "password": pwd,
-    "ssl_verification": False
-}
-my_files = Files(connection)
-##################################################################
-basepath = './'
-folders = os.listdir(basepath)
-ignored = ['.git','.github']
-#print(dir)
-for folder in folders:
-    path = os.path.join(basepath,folder)
-    if os.path.isdir(path):
-        if folder not in ignored:
-            pdstype = folder
-            for file in os.listdir(path):
-                memname = file.split('.')[0]
-                localfile = './' + folder + '/' + file
-                print(localfile)
-                pdsname = 'RGOPALK.RAG.' + pdstype + '(' + memname + ')'
-                print(pdsname)
-                rc = my_files.upload_file_to_dsn(localfile,pdsname)
-                print(rc)
-##########################################################################
-# my_jobs = Jobs(connection)
-# result = my_jobs.submit_from_mainframe('RGOPALK.JCL.CNTL(RUN)')
-# time.sleep(15)
-# rc = my_jobs.get_job_status(result['jobname'],result['jobid'])
-# print(rc)
+import subprocess
+command = "zowe zos-uss issue ssh /u/RGOPALK/runbuild.sh"
+# #command = "zowe --version"
+x = os.system(command)
+print(x)
 
