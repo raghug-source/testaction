fc9efe6ada839eed7678f9647f5a28a41d621d48
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-01 07:57:02 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
 import os
 import sys
 import subprocess
-command = "zowe zos-uss issue ssh /u/RGOPALK/runbuild.sh"
+hostname = sys.argv[1]
+hostport = sys.argv[2]
+userid = sys.argv[3]
+pwd = sys.argv[4]
+
+command = "zowe zos-uss issue ssh /u/RGOPALK/runbuild.sh --host hostname --port hostport --user userid --password pwd"
 # #command = "zowe --version"
 x = os.system(command)
 print(x)
