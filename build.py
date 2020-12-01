1acefc99e66862d0d86fbdacdb169a72464b339a
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-01 08:59:53 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,13 +1,17 @@
 import os
 import sys
-from subprocess import PIPE, run
+from subprocess import PIPE, Popen
 hostname = sys.argv[1]
 hostport = sys.argv[2]
 userid = sys.argv[3]
 pwd = sys.argv[4]
 
 command = "zowe zos-uss issue ssh /u/RGOPALK/runbuild.sh --host " + hostname +  ' --port ' + hostport + ' --user ' + userid + ' --password ' +  pwd
-result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-print(result.stdout)
+process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+print(process.communicate[0])
 
 
