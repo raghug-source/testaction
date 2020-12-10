1f9d1348122a144293ca8959ee0d98f301538650
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-10 19:14:33 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,17 +1,16 @@
-import os
-import sys
-from subprocess import PIPE, Popen
+# import os
+# import sys
+# from subprocess import PIPE, Popen
+import paramiko
 hostname = sys.argv[1]
 hostport = sys.argv[2]
 userid = sys.argv[3]
 pwd = sys.argv[4]
 
-command = "zowe zos-uss issue ssh /u/RGOPALK/runbuild.sh --host " + hostname +  ' --port ' + hostport + ' --user ' + userid + ' --password ' +  pwd
-process = Popen(
-        args=command,
-        stdout=PIPE,
-        shell=True
-    )
-print(process.communicate()[0])
-
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname,username=userid, password=pwd)
+cmd_to_execute = 'cd /u/RGOPALK;ls'
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
+print(ssh_stdout.read())
 
