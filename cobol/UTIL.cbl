5b8f308761a79563c557c4403a5e24ed8214e847
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-10 19:10:31 +0530

    Delete UTIL.cbl

--- cobol/UTIL.cbl
+++
@@ -1,9 +0,0 @@
-IDENTIFICATION DIVISION.                                   
-PROGRAM-ID.     UTIL.                                      
-                                                           
-DATA DIVISION.                                             
-   LINKAGE SECTION.                                        
-   COPY STUDCPY.                                           
-PROCEDURE DIVISION USING WS-STUDENT-ID, WS-STUDENT-NAME.   
-    DISPLAY 'IN RAMAUD PROGRAM'.                           
-    EXIT PROGRAM.                                          
