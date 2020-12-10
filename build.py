cb446f0dc10f5f08437dd94151f863850e2c1a5f
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-10 20:55:33 +0530

    Update build.py

--- build.py
+++ build.py
@@ -16,6 +16,6 @@ print(ssh_stdout.read())
 cmd_to_execute = 'git clone --branch feature2 git@github.com:raghug-source/testaction.git'
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
 print(ssh_stdout.read())
-cmd_to_execute = 'export JAVA_HOME=/usr/lpp/java/J8.0_64;export PATH=$JAVA_HOME/bin:$PATH;/usr/lpp/IBM/dbb/bin/groovyz /u/RGOPALK/zappbuild/origbuild.groovy --workspace /u/RGOPALK/zappbuild --application testaction --outDir /u/RGOPALK/zappbuild/out --hlq RGOPALK.RAG --fullBuild'
+cmd_to_execute = '. /etc/profile \n cd /u/RGOPALK/zappbuild/ \n git clone --branch feature2 git@github.com:raghug-source/testaction.git \n /usr/lpp/IBM/dbb/bin/groovyz /u/RGOPALK/zappbuild/origbuild.groovy --workspace /u/RGOPALK/zappbuild --application testaction --outDir /u/RGOPALK/zappbuild/out --hlq RGOPALK.RAG --fullBuild'
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
 print(ssh_stdout.read())
