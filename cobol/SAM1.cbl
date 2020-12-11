2ea102dd297c286da017b630a7abbc6f3005389e
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-11 11:03:50 +0530

    Update SAM1.cbl

--- cobol/SAM1.cbl
+++ cobol/SAM1.cbl
@@ -8,7 +8,7 @@
       ****************************************************************
       * PROGRAM:  SAM1
       *
-      * AUTHOR :  Douggie Stout
+      * AUTHOR :  Dougg Stout
       *
       * READS A SEQUENTIAL TRANSACTION FILE AND MAKES UPDATES
       * TO A SORTED SEQUENTIAL CUSTOMER FILE
