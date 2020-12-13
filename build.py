d2529ff529baea14bb4839c746f6f5c944aca402
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-12-13 17:59:50 +0530

    Update build.py

--- build.py
+++ build.py
@@ -18,4 +18,7 @@ ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
 print(ssh_stdout.read())
 cmd_to_execute = '. /etc/profile \n cd /u/RGOPALK/zappbuild/ \n git clone --branch feature2 git@github.com:raghug-source/testaction.git \n /usr/lpp/IBM/dbb/bin/groovyz /u/RGOPALK/zappbuild/origbuild.groovy --workspace /u/RGOPALK/zappbuild --application testaction --outDir /u/RGOPALK/zappbuild/out --hlq RGOPALK.RAG --fullBuild'
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-print(ssh_stdout.read())
+output_msg = ssh_stdout.read()
+print(output_msg)
+if '** Build State : ERROR' in output_msg:
+    raise SystemExit('Error: Build Ended in Error')
